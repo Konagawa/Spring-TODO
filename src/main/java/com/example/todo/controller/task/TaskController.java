c26d412ccc58c6f51d9c501c850b0d3bcc7e7a01
Konagawa <konashin215@me.com>
2024-09-27 10:59:31 +0900

    Form_Validation

--- src/main/java/com/example/todo/controller/task/TaskController.java
+++ src/main/java/com/example/todo/controller/task/TaskController.java
@@ -4,6 +4,8 @@ import com.example.todo.service.task.TaskService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -37,13 +39,20 @@ public class TaskController {
 
     // GET /tasks/creationForm
     @GetMapping("/creationForm")
-    public String showCreationFrom() {
-        return"tasks/form";
+    public String showCreationFrom(TaskForm form, Model model) {
+        if (form == null) {
+            form = new TaskForm(null, null, null);
+        }
+        model.addAttribute("taskForm", form);
+        return "tasks/form";
     }
 
     // POST /tasks
     @PostMapping
-    public String create(TaskForm form) {
+    public String create(@Validated TaskForm form, BindingResult bindingResult, Model model) {
+        if (bindingResult.hasErrors()) {
+            return showCreationFrom(form, model);
+        }
         taskService.create(form.toEntity());
         return "redirect:/tasks";
     }
