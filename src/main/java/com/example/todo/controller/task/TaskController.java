97ec084bab51ae1a07874e7f0ea9f398c5602bb9
Konagawa <konashin215@me.com>
2024-09-27 13:49:13 +0900

    Fix_Validation

--- src/main/java/com/example/todo/controller/task/TaskController.java
+++ src/main/java/com/example/todo/controller/task/TaskController.java
@@ -6,10 +6,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequiredArgsConstructor
@@ -39,19 +36,15 @@ public class TaskController {
 
     // GET /tasks/creationForm
     @GetMapping("/creationForm")
-    public String showCreationFrom(TaskForm form, Model model) {
-        if (form == null) {
-            form = new TaskForm(null, null, null);
-        }
-        model.addAttribute("taskForm", form);
+    public String showCreationFrom(@ModelAttribute TaskForm form) {
         return "tasks/form";
     }
 
     // POST /tasks
     @PostMapping
-    public String create(@Validated TaskForm form, BindingResult bindingResult, Model model) {
+    public String create(@Validated TaskForm form, BindingResult bindingResult) {
         if (bindingResult.hasErrors()) {
-            return showCreationFrom(form, model);
+            return showCreationFrom(form);
         }
         taskService.create(form.toEntity());
         return "redirect:/tasks";
